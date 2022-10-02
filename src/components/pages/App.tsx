92b5dc4e6a2cb4248f0bd7e76ef35b8e55ca3bdc
Regina Traber <regina.traber@isolutions.ch>
2022-10-02 19:34:11 +0200

    first changes for html layout

--- src/components/pages/App.tsx
+++ src/components/pages/App.tsx
@@ -1,5 +1,53 @@
 import './App.css'
 
 export const App = () => {
-  return <div>{/* TOOD: implement App */}</div>
+  return (
+    <>
+      <div className="title">ToDo App</div>
+      <div className="content">
+        <div>
+          <input type="text" placeholder="Aufgabe..."></input>
+          <button type="button">Hinzufügen</button>
+        </div>
+        <div>
+          <input type="checkbox" className="regularCheckbox"></input>
+          Alle Anzeigen
+        </div>
+        <div id="taskList">
+          <table>
+            <thead>
+              <tr>
+                <tr></tr>
+                <th>Wichtigkeit</th>
+                <th>Aufgabe</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>
+                  <input type="checkbox" className="regularCheckbox"></input>
+                </td>
+                <td>2</td>
+                <td>task1</td>
+                <td>
+                  <button type="button">Löschen</button>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <input type="checkbox" className="regularCheckbox"></input>
+                </td>
+                <td>3</td>
+                <td>task2</td>
+                <td>
+                  <button type="button">Löschen</button>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </>
+  )
 }
