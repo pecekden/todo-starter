cffaa4d6b1c381cb34b443a232ccf63c939f948c
Denis Pecekanovic <denis.pecekanovic@boydak.ch>
2022-11-20 06:29:02 +0100

    save commit

--- src/components/pages/Content.tsx
+++ src/components/pages/Content.tsx
@@ -1,4 +1,3 @@
-import { SortingColumn, SortingOrder } from 'models/Sorting'
 import { Todo } from 'models/Todo'
 import { useState } from 'react'
 import { InputSearch } from '../widgets/InputSearch'
@@ -10,8 +9,6 @@ export const Content = () => {
   const [todoInputText, setTodoInputText] = useState<string>('')
   const [showAll, setShowAll] = useState<boolean>(true)
   const [showExact, setShowExact] = useState<boolean>(true)
-  const [sortingColumn, setSortingColumn] = useState<SortingColumn>('None')
-  const [sortingOrder, setSortingOrder] = useState<SortingOrder>('unsorted')
 
   return (
     <>
@@ -30,10 +27,6 @@ export const Content = () => {
           showAll={showAll}
           showExact={showExact}
           todoInputText={todoInputText}
-          sortingColumn={sortingColumn}
-          setSortingColumn={setSortingColumn}
-          sortingOrder={sortingOrder}
-          setSortingOrder={setSortingOrder}
           todos={todos}
           setTodos={setTodos}
         ></TodoList>
